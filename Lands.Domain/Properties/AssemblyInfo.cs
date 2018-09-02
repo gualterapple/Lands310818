a1f9d782cfe7d91d43af27006a665b43233a713c
gualterapple <gualterapple@hotmail.com>
2018-09-02 13:21:53 -0300

    System.Web.Entity

--- Lands.Domain/Properties/AssemblyInfo.cs
+++ Lands.Domain/Properties/AssemblyInfo.cs
@@ -14,6 +14,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
+
+
 // Definir ComVisible como false torna os tipos neste assembly invisíveis
 // para componentes COM. Caso precise acessar um tipo neste assembly de
 // COM, defina o atributo ComVisible como true nesse tipo.
